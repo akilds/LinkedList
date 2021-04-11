5c182814e4da2ac7954604bc23804d44b5256b18
akilds <akilrainadesigan@gmail.com>
2021-04-11 12:15:13 +0530

    CREATED : LinkedList

--- MyNode.java
+++ MyNode.java
@@ -1,7 +1,22 @@
-public class MyNode
-{
- public static void main(String[] args)
- {
-  System.out.println("LinkedList");
- }
+public class MyNode<K> {
+
+	private K key;
+
+	private MyNode next;
+
+	public MyNode(K key){
+  	  this.key=key;
+     this.next=null;
+	}
+
+	public MyNode getNext()
+	{
+		return next;
+	}
+
+	public void setNext(MyNode next)
+	{
+		this.next = next;
+	}
 }
+
